d78e9b65b80b974b9d262f047bb54cdc33acdba1
Tatiana Viana <tatisabinoviana@gmail.com>
2021-05-28 22:26:39 -0300

    adição do imc de paulo

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,16 @@
-console.log("Fui carregado de um arquivo externo");
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura); // 100 / 2.0 * 2.0 = 100 / 4 =>>>>>> 25
+
+tdImc.textContent = imc;
